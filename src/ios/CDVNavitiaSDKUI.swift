9212e87f611e74a0b320ccb866daa72dd37d538b
Rachik ABIDI <rachik.abidi@nearteam.fr>
2018-08-07 11:02:19 +0200

    🚀 Release 1.4.0
    Android Min SDK is now 19

--- src/ios/CDVNavitiaSDKUI.swift
+++ src/ios/CDVNavitiaSDKUI.swift
@@ -91,6 +91,9 @@ import NavitiaSDKUI
         if (arguments["maxNbJourneys"] != nil) {
             params.maxNbJourneys = arguments["maxNbJourneys"] as? Int32 ?? 0
         }
+        if (arguments["bss_stands"] != nil) {
+            params.bssStands = arguments["bss_stands"] as? Bool ?? false
+        }
 
         return params
     }
