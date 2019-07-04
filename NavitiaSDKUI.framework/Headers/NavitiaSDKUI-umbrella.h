90e2a59be6568b9ae18bb2a4ad02170b944780e5
Guillaume <guillaume.lardillier@kisio.com>
2019-07-04 14:11:34 +0200

    Adding codeowners file

---
+++ NavitiaSDKUI.framework/Headers/NavitiaSDKUI-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Alamofire.h"
+#import "AlamofireObjectMapper.h"
+#import "ObjectMapper.h"
+#import "NavitiaSDKUI.h"
+
+FOUNDATION_EXPORT double NavitiaSDKUIVersionNumber;
+FOUNDATION_EXPORT const unsigned char NavitiaSDKUIVersionString[];
+
---
+++ NavitiaSDKUI.framework/Headers/NavitiaSDKUI.h
@@ -0,0 +1,16 @@
+//
+//  NavitiaSDKUI.h
+//  NavitiaSDKUI
+//
+//  Copyright © 2018 kisio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavitiaSDKUI.
+FOUNDATION_EXPORT double NavitiaSDKUIVersionNumber;
+
+//! Project version string for NavitiaSDKUI.
+FOUNDATION_EXPORT const unsigned char NavitiaSDKUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavitiaSDKUI/PublicHeader.h>
